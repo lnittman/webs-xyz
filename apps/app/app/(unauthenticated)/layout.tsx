82f8e4d1fe4280eff0a18c0b445e09961ef54c27
Luke Nittmann <luke.nittmann@gmail.com>
2025-05-21 01:47:50 -0400

    chore(app): remove placeholder content

--- apps/app/app/(unauthenticated)/layout.tsx
+++ apps/app/app/(unauthenticated)/layout.tsx
@@ -12,21 +12,11 @@ const AuthLayout = ({ children }: AuthLayoutProps) => (
       <div className="absolute inset-0 bg-zinc-900" />
       <div className="relative z-20 flex items-center font-medium text-lg">
         <CommandIcon className="mr-2 h-6 w-6" />
-        Acme Inc
       </div>
       <div className="absolute top-4 right-4">
         <ModeToggle />
       </div>
-      <div className="relative z-20 mt-auto">
-        <blockquote className="space-y-2">
-          <p className="text-lg">
-            &ldquo;This library has saved me countless hours of work and helped
-            me deliver stunning designs to my clients faster than ever
-            before.&rdquo;
-          </p>
-          <footer className="text-sm">Sofia Davis</footer>
-        </blockquote>
-      </div>
+      <div className="relative z-20 mt-auto" />
     </div>
     <div className="lg:p-8">
       <div className="mx-auto flex w-full max-w-[400px] flex-col justify-center space-y-6">
