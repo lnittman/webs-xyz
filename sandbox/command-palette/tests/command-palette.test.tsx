f32c14ce94b5341a4b35296e03b1dd9c2276eca3
Luke Nittmann <luke.nittmann@gmail.com>
2025-05-28 13:01:24 -0400

    work

--- sandbox/command-palette/tests/command-palette.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import { CommandPalette } from '../src/CommandPalette'
-
-describe('CommandPalette', () => {
-  it('renders without crashing', () => {
-    render(<CommandPalette />)
-  })
-})
